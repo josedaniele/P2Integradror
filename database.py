72d02ed43deaa07d8c44c2e989fceef4cb19b2f4
Mincicco <manuciriaco@gmail.com>
2022-10-12 13:00:48 -0300

    Funciones de la base de datos

---
+++ database.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+tabla = "CREATE TABLE IF NOT EXISTS MONOPATIN (ID INTEGER PRIMARY KEY, MARCA VARCHAR (20) UNIQUE, PRECIO FLOAT NOT NULL, CANTIDAD_DISPONIBLE INTEGER NOT NULL)"
+agregar_mono = "INSERT INTO MONOPATIN(marca,precio,cantidad-disponible) VALUES(?,?,?)"
+tabla_completa= "SELECT * FROM MONOPATIN"
+borrar = "DELETE FROM MONOPATIN WHERE id= ?"
+updt_precio = "UPDATE MONOPATIN SET precio =? WHERE id = ?"
+
+def conectar():
+    return sqlite3.connect("Datos_Monopatin.db")
+   
+
+def crear_tabla(conexion):
+    with conexion:
+        conexion.execute(tabla)
+   
+
+def insert_monopatin(conexion,marca,precio,cant_disponible):
+    with conexion:
+        conexion.execute(agregar_mono,(marca, precio, cant_disponible))
+
+def mostrar_tabla(conexion):
+    with conexion:
+        return conexion.execute(tabla_completa).fetchall()
+
+def eliminar_monopatin(conexion, id):
+    with conexion:
+        conexion.execute(borrar, (id))
+
+def modificar_precio(conexion, precio, id):
+    with conexion:
+        conexion.execute(updt_precio,(precio,id))
