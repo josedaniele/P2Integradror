c091c45bd97a930ef0824ca8018f4cd15f94b38e
josedaniele <josedaniele493@gmail.com>
2022-10-18 11:32:41 -0300

    menu principal e instancia de clase

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
+from ast import Num
 import database
 
-import database
+def menuPrincipal():
+    print("Bienvenido al sistema de cargas de Monopatines")
+    opcion:int = 5
+    while opcion > 4 or opcion <1:
+        print(
+            "1.Cargar monopatin\n"
+            "2.Mostrar monopatines\n"
+            "3.Eliminar monopatin\n"
+            "4.Modificar precios\n"
+        )
+        try :
+            opcion =int(input("Ingrese un numero en base a la opcion: "))
+            if opcion == 1:
+                print("cargarMonopatin()")
+            elif opcion == 2:
+                print("mostrarMonopatines()")
+            elif opcion == 3 :
+                print("eliminarMonopatin()")
+            elif opcion == 4:
+                print("modificarPrecio()")
+            else:
+                print("Ingrese un numero dentro de las opciones")
+        except ValueError:
+            print("Valor invalido, ingrese un numero")
+menuPrincipal()
---
+++ class.py
@@ -0,0 +1,36 @@
+
+class Monopatin():
+    def __init__(self, marca, precio, cantidad):
+        self.marca = marca
+        self.precio = precio
+        self.cantidad = cantidad
+
+    def cargarDatos(self):
+
+        self.marca = 
+        self.precio = 
+        self.cantidad = 
+
+    def mostrarDatos(self):
+        print(f"{self.marca} {self.precio} {self.cantidad}")
+
+
+def cargarDatos():
+    Valido = True
+    marca: str = input("Ingrese la marca ").upper()
+    while Valido == True:
+        try:
+            precio = float(input("Ingrese el precio "))
+            Valido = False
+        except ValueError:
+            print("Precio no valido introduzca un numero")
+    Valido = True
+    while Valido == True:
+        try:
+            cantidad = int(input("Ingrese la cantidad disponible "))
+            Valido = False
+        except ValueError:
+            print("Cantidad no valido introduzca un numero")
+
+
+cargarDatos()
